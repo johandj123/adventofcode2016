af6dedbbb7ffb70df43d43944c38e48fd91d1720
Johan de Jong <johan.de.jong@sogeti.com>
2024-03-17 14:26:22 +0100

    Day 19 part 2.

--- src/Day19.java
+++ src/Day19.java
@@ -1,21 +1,51 @@
 import lib.InputUtil;
 
 import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
 
 public class Day19 {
     public static void main(String[] args) throws IOException {
         int input = Integer.parseInt(InputUtil.readAsString("input19.txt"));
-        Elf firstElf = buildElfCircle(input);
-        first(firstElf);
+        first(input);
+        second(input);
     }
 
-    private static void first(Elf firstElf) {
-        Elf elf = firstElf;
+    private static void first(int input) {
+        Elf elf = buildElfCircle(input);
         while (elf.nextElf != elf) {
             elf.nextElf = elf.nextElf.nextElf;
             elf = elf.nextElf;
         }
         System.out.println(elf.number);
+    }
+
+    private static void second(int input) {
+        Deque<Integer> left = new ArrayDeque<>();
+        Deque<Integer> right = new ArrayDeque<>();
+        for (int i = 1; i <= input / 2; i++) {
+            left.addLast(i);
+        }
+        for (int i = (input / 2) + 1; i <= input; i++) {
+            right.addFirst(i);
+        }
+
+        while (left.size() + right.size() > 1) {
+            if (left.size() > right.size()) {
+                left.removeLast();
+            } else {
+                right.removeLast();
+            }
+            right.addFirst(left.removeFirst());
+            left.addLast(right.removeLast());
+        }
+
+        List<Integer> result = new ArrayList<>();
+        result.addAll(left);
+        result.addAll(right);
+        System.out.println(result.get(0));
     }
 
     private static Elf buildElfCircle(int input) {
