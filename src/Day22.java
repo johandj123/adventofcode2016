f12a21bf2c1d4320c96a3049b93b1f20443e1c40
Johan de Jong <johan.de.jong@sogeti.com>
2024-03-18 17:55:46 +0100

    Day 22 part 2.

--- src/Day22.java
+++ src/Day22.java
@@ -10,6 +10,11 @@ public class Day22 {
                 .filter(s -> s.startsWith("/"))
                 .map(Node::new)
                 .collect(Collectors.toList());
+        first(nodes);
+        second(nodes);
+    }
+
+    private static void first(List<Node> nodes) {
         int count = 0;
         for (int i = 0; i < nodes.size(); i++) {
             Node a = nodes.get(i);
@@ -29,11 +34,43 @@ public class Day22 {
         System.out.println(count);
     }
 
+    private static void second(List<Node> nodes) {
+        int width = nodes.stream().mapToInt(node -> node.x).max().orElseThrow() + 1;
+        int height = nodes.stream().mapToInt(node -> node.y).max().orElseThrow() + 1;
+        Node[][] layout = new Node[height][width];
+        for (Node node : nodes) {
+            layout[node.y][node.x] = node;
+        }
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int size = layout[y][x].used + layout[y][x].avail;
+                int used = layout[y][x].used;
+                if (used == 0) {
+                    System.out.printf("__/%02d ", size);
+                } else if (size >= 100) {
+                    System.out.print("##### ");
+                } else {
+                    System.out.printf("%02d/%02d ", used, size);
+                }
+            }
+            System.out.println();
+        }
+        // Answer determined manually from looking at the data
+        // 61 moves to get the empty node to the goal data
+        // moving the target data left takes 5 moves per node, move 30 nodes
+        System.out.println(61+30*5);
+    }
+
     static class Node {
+        final int x;
+        final int y;
         final int used;
         final int avail;
 
         public Node(String s) {
+            List<Integer> integers = InputUtil.extractPositiveIntegers(s);
+            x = integers.get(0);
+            y = integers.get(1);
             used = Integer.parseInt(s.substring(30, 33).trim());
             avail = Integer.parseInt(s.substring(37, 40).trim());
         }
